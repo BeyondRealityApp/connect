69a32c7069afbaa3df973375ad630ab59c2a6cfc
MichielvanBeers <michielvanbeers@gmail.com>
2024-12-03 14:17:09 +0100

    Remove development comments

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,11 +12,10 @@ export default function Home() {
 
   return (
     <>
-      <WizardController />
-      {/* {online && <LandingController />}
+      {online && <LandingController />}
       {!online && status === "notStarted" && <WizardController />}
       {!online && status === "inProgress" && <QuestionsController />}
-      {!online && status === "completed" && <FinalController />} */}
+      {!online && status === "completed" && <FinalController />}
     </>
   );
 }
